f80f71f958a5e8f9ced5a308fefeb23708d5981d
Nishtha <148875630+Nishtha56@users.noreply.github.com>
2025-04-07 23:54:39 +0530

    main.py

--- main.py
+++ main.py
@@ -2,4 +2,18 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-file_path=read._excel("")
+#Load Dataset
+file_path="Market_Resercher_excel.xlsx"
+data=pd.read_excel(file_path, sheet_name='market_researcher_dataset')
+print("Load Successfully")
+
+#Cleaning of the data
+#Data Information
+# Gives the information about the data
+data.info()
+
+#Perform the statistical function on the data
+print(data.describe())
+
+#Is any null values are present or not if it is present then print how many null values are there 
+print("Total null values: \n",data.isnull().sum())
